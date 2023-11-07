2bfad0129a6cb724ba78bd0ef73916bb320a6893
deniz0199 <deniz.nal@ozu.edu.tr>
2023-11-07 19:39:25 +0300

    should work with mouse now

--- src/mouse-node-deniz0199/src/publisher_node.cpp
+++ src/mouse-node-deniz0199/src/publisher_node.cpp
@@ -1,19 +1,31 @@
 #include "ros/ros.h"
-
-#include "std_msgs/String.h"
+#include "std_msgs/Char.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "Publisher");
     ros::NodeHandle nh;
 
-    ros::Publisher topic_pub = nh.advertise<std_msgs::String>("mouse_data",10000000);
+    ros::Publisher topic_pub = nh.advertise<std_msgs::Char>("mouse_node",999);
     ros::Rate loop_rate(1);
 
-    while(ros::ok()) {
-        std_msgs::String msg;
-        msg.data = "Hello World!";
+    char mouseMvmData[3];
 
-        topic_pub.publish(msg);
+    int file = open("/dev/input/mice", O_RDONLY);
+
+    while(ros::ok()) {
+
+        read(file, mouseMvmData, 3);
+
+        std_msgs::Char x;
+        std_msgs::Char y;
+
+        x.data = mouseMvmData[1];
+        y.data = mouseMvmData[2];
+
+        topic_pub.publish(x);
         ros::spinOnce();
         loop_rate.sleep();
     }
